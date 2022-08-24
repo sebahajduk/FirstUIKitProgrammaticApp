633afd2be7b93a9c8b6cdf66cfcebf91ba5629df
sebahajduk <jiuzeer@gmail.com>
2022-08-24 17:38:34 +0200

    Add feature: scroll followers list, pagination, searching textfield

--- FirstUIKitProgrammaticApp/Managers/NetworkManager.swift
+++ FirstUIKitProgrammaticApp/Managers/NetworkManager.swift
@@ -5,4 +5,53 @@
 //  Created by Sebastian Hajduk on 16/08/2022.
 //
 
-import Foundation
+import UIKit
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    private let baseURL = "https://api.github.com/users/"
+    
+    let cache = NSCache<NSString, UIImage>()
+    
+    private init() { }
+    
+    func getFollowers(for username: String, page: Int, completed: @escaping (Result<[Follower], GFError>) -> Void) {
+        let endpoint = baseURL + "\(username)/followers?per_page=100&page=\(page)"
+        
+        guard let url = URL(string: endpoint) else {
+            completed(.failure(.invalidUsername))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let _ = error {
+                completed(.failure(.unableToComplete))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completed(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let followers = try decoder.decode([Follower].self, from: data)
+                completed(.success(followers))
+            } catch {
+                completed(.failure(.invalidData))
+            }
+
+        }
+        
+        task.resume()
+    }
+    
+}
