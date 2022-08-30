cdecf720c9d689805d84e7deb6fe670de68e817b
sebahajduk <jiuzeer@gmail.com>
2022-08-30 19:42:38 +0200

    Add favorites list

--- FirstUIKitProgrammaticApp/Managers/PersistenceManager.swift
+++ FirstUIKitProgrammaticApp/Managers/PersistenceManager.swift
@@ -38,13 +38,15 @@ enum PersistenceManager {
                     retrievedFavorites.removeAll { $0.login == favorite.login}
                 }
                 
-                completed(save(favorites: favorites))
+                completed(save(favorites: retrievedFavorites))
                 
             case .failure(let error):
                 completed(error)
             }
         }
     }
+    
+    
     
     static func retrieveFavorites(completed: @escaping (Result<[Follower], GFError>) -> Void) {
         guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else {
